7af8a143063b7ef315d76621a6c6eabf1640aeaa
ruben <rubenvanstaden@gmail.com>
2022-06-30 08:56:46 -0400

    feat: generate sbatch script for SLURM job submission

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-	"log"
 	"os"
+	"log"
 	"text/template"
 )
 
 var temp *template.Template
 
-type MasterNode struct {
-    Path        string
-    Name        string
-    Image       string
-    TmpDir      string
-    PreScripts  []string
-    PostScripts []string
+type ClusterConfig struct {
+    Path          string
+    GlobalData    string
+    GlobalScratch string
+    LocalData     string
+    LocalScratch  string
 }
 
-type ComputeNode struct {
-    Path      string
-    Name      string
-    Image     string
-    TmpDir    string
-    Script    string
-    Scratch   string
-    Artifacts map[string]string
+type UserConfig struct {
+    JobId      string
+    Image      string
+    TmpDir     string
+    Artifacts  map[string]string
+    PreScript  []string
+    JobScript  string
+    PostScript []string
 }
 
-type Data struct {
-    Master MasterNode
-    Nodes  []ComputeNode
+type Config struct {
+    Cluster ClusterConfig
+    User    UserConfig
 }
 
 func init() {
@@ -38,13 +37,16 @@ func init() {
 
 func main() {
 
-    master := MasterNode{
-        Path: "/var/tmp/cluster/",
-        Name: "master",
-        Image: "mongo:latest",
-        TmpDir: "/scratch/local/",
-        PreScripts: []string{"echo 'run pre-script...'"},
-        PostScripts: []string{"echo 'run post-script...'"},
+    // 1. one job equals one image
+    // 2. one job equals one srun script (job script)
+
+    // $GLOBAL_SCRATCH/<JOB_ID>/cfd-results  # store artifacts from local scratch
+    // $GLOBAL_SCRATCH/<JOB_ID>/em-results   # store artifacts
+    // $LOCAL_DATA/<JOB_ID>                  # unpack charlie cloud image
+    // $LOCAL_SCRATCH/<JOB_ID>               # store temp job run data
+
+    variables := map[string]string{
+        "TMPDIR": "/var/tmp/scratch",
     }
 
     artifacts := map[string]string{
@@ -52,32 +54,30 @@ func main() {
         "em-results": "/var/tmp/em-results",
     }
 
-    node1 := ComputeNode{
-        Path: "/var/tmp/cluster/",
-        Name: "node1",
+    cluster := ClusterConfig{
+        Path:          "/var/tmp/cluster/",
+        GlobalData:    "share/thunderhead/data/",
+        GlobalScratch: "share/thunderhead/scratch/",
+        LocalData:     "local/thunderhead/data/",
+        LocalScratch:  "local/thunderhead/scratch/tmp/",
+    }
+
+    user := UserConfig{
+        JobId: "12345667789",
         Image: "mongo:latest",
-        TmpDir: "/scratch/local/",
-        Script: "hello/hello.sh",
-        Scratch: "/var/tmp/scratch",
+        TmpDir: variables["TMPDIR"],
         Artifacts: artifacts,
+        PreScript: []string{"echo 'run pre-script...'"},
+        JobScript: "hello/hello.sh",
+        PostScript: []string{"echo 'run post-script...'"},
     }
 
-    node2 := ComputeNode{
-        Path: "/var/tmp/cluster/",
-        Name: "node2",
-        Image: "mongo:latest",
-        TmpDir: "/scratch/local/",
-        Script: "hello/hello.sh",
-        Scratch: "/var/tmp/scratch",
-        Artifacts: artifacts,
+    config := Config{
+        Cluster: cluster,
+        User: user,
     }
 
-    data := Data{
-        Master: master,
-        Nodes:  []ComputeNode{node1, node2},
-    }
-
-    err := temp.Execute(os.Stdout, data)
+    err := temp.Execute(os.Stdout, config)
     if err != nil {
         log.Fatalln(err)
     }
--- templates/charliecloud.sh
+++ templates/charliecloud.sh
@@ -2,43 +2,33 @@
 
 set -ex
 
-JOB_ID=$1
-
-th started $JOB_ID
+th started {{.User.JobId}}
 
 # --------------------------------------------
 
-MASTER_IMAGE={{.Master.Path}}{{.Master.Name}}{{.Master.TmpDir}}$JOB_ID
+IMAGE={{.Cluster.Path}}{{.Cluster.LocalData}}{{.User.JobId}}
+TMPDIR={{.Cluster.Path}}{{.Cluster.LocalScratch}}{{.User.JobId}}
+ARTIFACTS={{.Cluster.Path}}{{.Cluster.GlobalScratch}}{{.User.JobId}}
 
-ch-convert -i docker -o dir {{.Master.Image}} $MASTER_IMAGE
+# srun
+ch-convert -i docker -o dir {{.User.Image}} $IMAGE
 
-{{range .Master.PreScripts -}}
-    ch-run $MASTER_IMAGE -- {{.}}
+{{range .User.PreScript -}}
+    ch-run $IMAGE -- {{. -}}
 {{end}}
 
-{{range .Nodes -}}
+# srun
+ch-run \
+    --bind=$TMPDIR:{{.User.TmpDir}} \
+    {{range $src, $dst := .User.Artifacts -}}
+        --bind=$ARTIFACTS/{{$src}}:{{$dst}} \
+    {{end -}}
+    $IMAGE -- {{.User.JobScript}}
 
-    workspace={{.Path}}{{.Name}}{{.TmpDir}}$JOB_ID
-
-    IMAGE=$workspace/image
-    TMP=$workspace/tmp
-    ARTIFACTS=$workspace/artifacts
-
-    ch-convert -i docker -o dir {{.Image}} $IMAGE
-
-    ch-run \
-        --bind=$TMP:{{.Scratch}} \
-        {{range $src, $dst := .Artifacts -}}
-            --bind=$ARTIFACTS/{{$src}}:{{$dst}} \
-        {{end -}}
-        $IMAGE -- {{.Script}}
-
-{{end}}
-
-{{range .Master.PostScripts -}}
-    ch-run $MASTER_IMAGE -- {{.}}
+{{range .User.PostScript -}}
+    ch-run $IMAGE -- {{. -}}
 {{end}}
 
 # --------------------------------------------
 
-th completed $JOB_ID
+th completed {{.User.JobId}}
